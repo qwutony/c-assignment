5a1eb87396b55fbfd0b9d344d6497acc1a95159f
qwutony <45024645+qwutony@users.noreply.github.com>
2018-12-18 23:34:45 -0500

    fake print add

--- strings.c
+++ strings.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+void fake_print(char *string) {
+    
+    int i = 0;
+
+    do {
+        i++;
+        printf("%c", string[i]);
+    }
+    while (string[i] != 0);
+    printf("\n");
+}
+
 int main(int argc, char const *argv[]) {   
     
     // manual strings!
@@ -10,18 +22,19 @@ int main(int argc, char const *argv[]) {
     char manual_strings[15] = {'h', 'e', 'l', 'l', 'o', 32, 
                             'w', 'o', 'r', 'l', 'd', '\n', 0};
 
-    printf(manual_strings);
-
+    // printf(manual_strings);
+    fake_print(manual_strings);
+    int x = 47;
     // a char is not a letter or number, but it is a smaller integer
     // char easy_string = "abcd";
 
-    char *easy_string = "abcd";
+    // char *easy_string = "abcd";
 
-    for (int i = 0; i < 20; i++) {
+    // for (int i = 0; i < 20; i++) {
 
-        // can still use array syntax even if not array
-        printf("%4d %4c %4x\n", easy_string[i], easy_string[i], easy_string[i]);
-    }
+    //     // can still use array syntax even if not array
+    //     printf("%4d %4c %4x\n", easy_string[i], easy_string[i], easy_string[i]);
+    // }
 
 
     return 0;
