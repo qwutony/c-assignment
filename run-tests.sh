0ee4b007b748e4b01f3e287696cb40c8760394c8
qwutony <45024645+qwutony@users.noreply.github.com>
2019-01-10 21:28:33 -0500

    automated entering

--- run-tests.sh
+++ run-tests.sh
@@ -4,5 +4,5 @@
 # > means to write program's output into file
 # this is called "redirection"
 
-build/hexdump hexdump-test-text > textOutput.txt
-build/hexdump hexdump-wikipedia.ico > wikiOutput.txt
+yes | build/hexdump hexdump-test-text > textOutput.txt
+yes | build/hexdump hexdump-wikipedia.ico > wikiOutput.txt
